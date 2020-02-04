a5b3791b0502ca308249f7e79b3f0df13894b63c
hooncp <hooncheep@gmail.com>
2020-02-04 13:20:16 +0800

    Improve design of footer

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ function App() {
     <div className = "App">
     <Explore/>
     <Popular/>
-    <Footer/>
+
     </div>
+    <Footer/>
     </div>
   )
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,36 +1,46 @@
 import React, { Component } from 'react'
 import icon from '../../Assets/icon2.png'
+import "./footer.css"
 
 
 export default class Footer extends Component {
     render() {
         return (
-    <footer className="footer">
-            <div className = "col-md-6">
-            <a className="navbar-brand" href="#">
-                  <img src = {icon}/>
-            </a>
-            </div>
-            <div className = "col-md-6">
-                <ul className="list-inline">
-                <li className="list-inline-item">
-              <a href="#">FAQ</a>
-            </li>
-            <li className="list-inline-item">
-              <a href="#">About</a>
-            </li>
-            <li className="list-inline-item">
-              <a href="#">Contact us</a>
-            </li>
-            <li className="list-inline-item">
-              <a href="#">How it works</a>
-            </li>
+        <footer className="footer bg-light">
+              <div className = "row">
+                <div className = "col-sm-4">
+                  <a href="#">
+                    <img src = {icon}/>
+                  </a>
+                  <p className="py-2 text-muted small ">&copy; Presend 2020. All Rights Reserved.</p>
+                </div>
+                <div className = "col-sm-4">
+                  <h4 className = "font-weight-bold">About</h4>
+                  <div className = "py-2">
+                  <a href="#">FAQ</a>
+                  </div>
+                  <div className = "py-2">
+                  <a href="#">About</a>
+                  </div>
+                  <div className = "py-2">
+                  <a href="#">Contact us</a>
+                  </div>
+                  <div className = "py-2">
+                  <a href="#">How it works</a>
+                  </div>
+                </div>
+                <div className = "col-sm-4">
+                <h4 className = "font-weight-bold">Contact Us</h4>
+                  <div className = "py-2">
+                  <a href="#">Facebook</a>
+                  </div>
+                  <div className = "py-2">
+                  <a href="#">Instagram</a>
+                  </div>
+                </div>
+              </div>
 
-          </ul>
-          <p className="text-muted small ">&copy; Presend 2020. All Rights Reserved.</p>
-        </div>
-            
-  </footer>
+        </footer>
         )
     }
 }
